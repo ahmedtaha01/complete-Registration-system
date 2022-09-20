336de2501fff803b137211fe70dd77fcdbbbc666
ahmedtaha01 <ahmedmohamedtaha2001@gmail.com>
2022-09-20 02:07:15 +0200

    donation models

--- donation/models.py
+++ donation/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from project.models import Project
 from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
 class Donation(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE,related_name='donations')
     user = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
-    amount = models.IntegerField()
+    amount = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(250000)])
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.amount
+        return self.project.project_title
